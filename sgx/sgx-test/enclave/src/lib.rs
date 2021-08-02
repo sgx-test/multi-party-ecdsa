f0f65d15bd724702f0504e242ebaa821888d4d5a
prz23 <panruizheng@zju.edu.cn>
2021-08-02 12:13:58 +0800

    add key gen stage 1 test

--- sgx/sgx-test/enclave/src/lib.rs
+++ sgx/sgx-test/enclave/src/lib.rs
@@ -78,7 +78,7 @@ pub extern "C" fn say_something(some_string: *const u8, some_len: usize) -> sgx_
     // Ocall to normal world for output
     println!("{}", &hello_string);
 
-    multi_party_ecdsa::sign();
+    tests::key_gen_stage_1();
 
     sgx_status_t::SGX_SUCCESS
 }
--- sgx/sgx-test/enclave/src/tests.rs
+++ sgx/sgx-test/enclave/src/tests.rs
@@ -1,10 +1,15 @@
 use multi_party_ecdsa::*;
 
+use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2020::orchestrate::*;
+
 use std::prelude::v1::*;
 use std::panic::{catch_unwind, resume_unwind, AssertUnwindSafe};
 
 //#[test]
-pub fn sign() {
-
+pub fn key_gen_stage_1() {
+     let stage1_input = KeyGenStage1Input{
+         index : 0usize,
+     };
+     let res = keygen_stage1(&stage1_input);
 }
 
